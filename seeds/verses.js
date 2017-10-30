011a326ee89fee156a71d641b8cf2c19dfd9135b
Matthew Hummer <biggreyson@gmail.com>
2017-10-29 23:11:41 -0600

    add 4 nephi

--- seeds/verses.js
+++ seeds/verses.js
@@ -2,7 +2,8 @@ let ne1 = require('../verses/1ne.js');
 let enos = require('../verses/enos.js');
 let jar = require('../verses/jar.js');
 let omni = require('../verses/omni.js');
-let verses = [...ne1,...enos,...jar,...omni];
+let ne4 = require('../verses/4ne.js');
+let verses = [...ne1,...enos,...jar,...omni,...ne4];
 
 exports.seed = function(knex, Promise) {
   return knex('verses').del()
