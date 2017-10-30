3b835dcd005ed9979c04f66b902b0be2db4dec1c
Matthew Hummer <biggreyson@gmail.com>
2017-10-29 22:30:59 -0600

    add enos

--- seeds/verses.js
+++ seeds/verses.js
@@ -1,5 +1,6 @@
 let ne1 = require('../verses/1ne.js');
-let verses = [...ne1];
+let enos = require('../verses/enos.js');
+let verses = [...ne1,...enos];
 
 exports.seed = function(knex, Promise) {
   return knex('verses').del()
