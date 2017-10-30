92111f05acbb436ca6e90c58ba433b219e3388d5
Matthew Hummer <biggreyson@gmail.com>
2017-10-29 22:56:07 -0600

    add omni

--- seeds/verses.js
+++ seeds/verses.js
@@ -1,7 +1,8 @@
 let ne1 = require('../verses/1ne.js');
 let enos = require('../verses/enos.js');
 let jar = require('../verses/jar.js');
-let verses = [...ne1,...enos,...jar];
+let omni = require('../verses/omni.js');
+let verses = [...ne1,...enos,...jar,...omni];
 
 exports.seed = function(knex, Promise) {
   return knex('verses').del()
