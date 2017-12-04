61672c951d355aa4f242c053fe8f235da8036a47
Matthew Hummer <biggreyson@gmail.com>
2017-12-04 10:32:49 -0700

    2 nephi 11

--- verses/2ne.js
+++ verses/2ne.js
@@ -1826,6 +1826,64 @@ module.exports = [
   },
 
 
+
+
+
+
+
+
+
+
+  {
+    book_id: 2,
+    text: "And now, Jacob spake many more things to my people at that time; nevertheless only these things have I caused to be written, for the things which I have written sufficeth me.",
+    verse: 1,
+    chapter: 11
+  },
+  {
+    book_id: 2,
+    text: "And now I, Nephi, write more of the words of Isaiah, for my soul delighteth in his words. For I will liken his words unto my people, and I will send them forth unto all my children, for he verily saw my Redeemer, even as I have seen him.",
+    verse: 2,
+    chapter: 11
+  },
+  {
+    book_id: 2,
+    text: "And my brother, Jacob, also has seen him as I have seen him; wherefore, I will send their words forth unto my children to prove unto them that my words are true. Wherefore, by the words of three, God hath said, I will establish my word. Nevertheless, God sendeth more witnesses, and he proveth all his words.",
+    verse: 3,
+    chapter: 11
+  },
+  {
+    book_id: 2,
+    text: "Behold, my soul delighteth in proving unto my people the truth of the coming of Christ; for, for this end hath the law of Moses been given; and all things which have been given of God from the beginning of the world, unto man, are the typifying of him.",
+    verse: 4,
+    chapter: 11
+  },
+  {
+    book_id: 2,
+    text: "And also my soul delighteth in the covenants of the Lord which he hath made to our fathers; yea, my soul delighteth in his grace, and in his justice, and power, and mercy in the great and eternal plan of deliverance from death.",
+    verse: 5,
+    chapter: 11
+  },
+  {
+    book_id: 2,
+    text: "And my soul delighteth in proving unto my people that save Christ should come all men must perish.",
+    verse: 6,
+    chapter: 11
+  },
+  {
+    book_id: 2,
+    text: "For if there be no Christ there be no God; and if there be no God we are not, for there could have been no creation. But there is a God, and he is Christ, and he cometh in the fulness of his own time.",
+    verse: 7,
+    chapter: 11
+  },
+  {
+    book_id: 2,
+    text: "And now I write some of the words of Isaiah, that whoso of my people shall see these words may lift up their hearts and rejoice for all men. Now these are the words, and ye may liken them unto you and unto all men.",
+    verse: 8,
+    chapter: 11
+  },
+
+
 ];
 
 let template = {
