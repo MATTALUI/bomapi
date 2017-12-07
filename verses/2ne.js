7b23741bc5983ae3b3ecd569ad961e3c6394d633
Matthew Hummer <biggreyson@gmail.com>
2017-12-06 21:23:23 -0700

    2 nephi 16

--- verses/2ne.js
+++ verses/2ne.js
@@ -2426,6 +2426,94 @@ module.exports = [
     chapter: 15
   },
 
+
+
+
+
+
+
+
+
+
+  {
+    book_id: 2,
+    text: "In the year that king Uzziah died, I saw also the Lord sitting upon a throne, high and lifted up, and his train filled the temple.",
+    verse: 1,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "Above it stood the seraphim; each one had six wings; with twain he covered his face, and with twain he covered his feet, and with twain he did fly.",
+    verse: 2,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "And one cried unto another, and said: Holy, holy, holy, is the Lord of Hosts; the whole earth is full of his glory.",
+    verse: 3,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "And the posts of the door moved at the voice of him that cried, and the house was filled with smoke.",
+    verse: 4,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "Then said I: Wo is unto me! for I am undone; because I am a man of unclean lips; and I dwell in the midst of a people of unclean lips; for mine eyes have seen the King, the Lord of Hosts.",
+    verse: 5,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "Then flew one of the seraphim unto me, having a live coal in his hand, which he had taken with the tongs from off the altar;",
+    verse: 6,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "And he laid it upon my mouth, and said: Lo, this has touched thy lips; and thine iniquity is taken away, and thy sin purged.",
+    verse: 7,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "Also I heard the voice of the Lord, saying: Whom shall I send, and who will go for us? Then I said: Here am I; send me.",
+    verse: 8,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "And he said: Go and tell this people—Hear ye indeed, but they understood not; and see ye indeed, but they perceived not.",
+    verse: 9,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "Make the heart of this people fat, and make their ears heavy, and shut their eyes—lest they see with their eyes, and hear with their ears, and understand with their heart, and be converted and be healed.",
+    verse: 10,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "Then said I: Lord, how long? And he said: Until the cities be wasted without inhabitant, and the houses without man, and the land be utterly desolate;",
+    verse: 11,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: "And the Lord have removed men far away, for there shall be a great forsaking in the midst of the land.",
+    verse: 12,
+    chapter: 16
+  },
+  {
+    book_id: 2,
+    text: " But yet there shall be a tenth, and they shall return, and shall be eaten, as a teil tree, and as an oak whose substance is in them when they cast their leaves; so the holy seed shall be the substance thereof.",
+    verse: 13,
+    chapter: 16
+  },
+
 ];
 
 let template = {
