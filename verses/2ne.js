a0c9eeab6791816105dca499595139580225398b
Matthew Hummer <biggreyson@gmail.com>
2017-12-08 12:13:23 -0700

    2 nephi 18

--- verses/2ne.js
+++ verses/2ne.js
@@ -2674,6 +2674,148 @@ module.exports = [
     chapter: 17
   },
 
+
+
+
+
+
+
+
+
+
+  {
+    book_id: 2,
+    text: "Moreover, the word of the Lord said unto me: Take thee a great roll, and write in it with a man’s pen, concerning Maher-shalal-hash-baz.",
+    verse: 1,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "And I took unto me faithful witnesses to record, Uriah the priest, and Zechariah the son of Jeberechiah.",
+    verse: 2,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "And I went unto the prophetess; and she conceived and bare a son. Then said the Lord to me: Call his name, Maher-shalal-hash-baz.",
+    verse: 3,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "For behold, the child shall not have knowledge to cry, My father, and my mother, before the riches of Damascus and the spoil of Samaria shall be taken away before the king of Assyria.",
+    verse: 4,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "The Lord spake also unto me again, saying:",
+    verse: 5,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "Forasmuch as this people refuseth the waters of Shiloah that go softly, and rejoice in Rezin and Remaliah’s son;",
+    verse: 6,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "Now therefore, behold, the Lord bringeth up upon them the waters of the river, strong and many, even the king of Assyria and all his glory; and he shall come up over all his channels, and go over all his banks.",
+    verse: 7,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "And he shall pass through Judah; he shall overflow and go over, he shall reach even to the neck; and the stretching out of his wings shall fill the breadth of thy land, O Immanuel.",
+    verse: 8,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "Associate yourselves, O ye people, and ye shall be broken in pieces; and give ear all ye of far countries; gird yourselves, and ye shall be broken in pieces; gird yourselves, and ye shall be broken in pieces.",
+    verse: 9,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "Take counsel together, and it shall come to naught; speak the word, and it shall not stand; for God is with us.",
+    verse: 10,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "For the Lord spake thus to me with a strong hand, and instructed me that I should not walk in the way of this people, saying:",
+    verse: 11,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "Say ye not, A confederacy, to all to whom this people shall say, A confederacy; neither fear ye their fear, nor be afraid.",
+    verse: 12,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "Sanctify the Lord of Hosts himself, and let him be your fear, and let him be your dread.",
+    verse: 13,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "And he shall be for a sanctuary; but for a stone of stumbling, and for a rock of offense to both the houses of Israel, for a gin and a snare to the inhabitants of Jerusalem.",
+    verse: 14,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "And many among them shall stumble and fall, and be broken, and be snared, and be taken.",
+    verse: 15,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "Bind up the testimony, seal the law among my disciples.",
+    verse: 16,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "And I will wait upon the Lord, that hideth his face from the house of Jacob, and I will look for him.",
+    verse: 17,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "Behold, I and the children whom the Lord hath given me are for signs and for wonders in Israel from the Lord of Hosts, which dwelleth in Mount Zion.",
+    verse: 18,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "And when they shall say unto you: Seek unto them that have familiar spirits, and unto wizards that peep and mutter—should not a people seek unto their God for the living to hear from the dead?",
+    verse: 19,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "To the law and to the testimony; and if they speak not according to this word, it is because there is no light in them.",
+    verse: 20,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "And they shall pass through it hardly bestead and hungry; and it shall come to pass that when they shall be hungry, they shall fret themselves, and curse their king and their God, and look upward.",
+    verse: 21,
+    chapter: 18
+  },
+  {
+    book_id: 2,
+    text: "And they shall look unto the earth and behold trouble, and darkness, dimness of anguish, and shall be driven to darkness.",
+    verse: 22,
+    chapter: 18
+  },
+
 ];
 
 let template = {
